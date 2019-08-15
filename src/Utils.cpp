59060b57735ba7e3febf678a578fec78556c948d
shibohan <tuotuogzs.ddns.net>
2019-08-14 20:57:46 -0400

    Change To GNU License

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,7 +1,21 @@
-/* Copyright (C) DroneOA Group - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+/* Copyright (C) 2019 DroneOA Group - All Rights Reserved
+ * This file is part of DroneOA_ROS.
+ *
+ * DroneOA_ROS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * DroneOA_ROS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with DroneOA_ROS. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
  * Written by Bohan Shi <b34shi@edu.uwaterloo.ca>, August 2019
+ * All Reference Attached
  */
 
 #include <droneoa_ros/Utils.hpp>
