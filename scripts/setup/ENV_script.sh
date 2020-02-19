2e67531f04bb8c6ea173a90ba04f584fc9d3ca98
DanielXu77 <xuyiwei1998@gmail.com>
2020-02-18 22:24:32 -0500

    clear directory before init

--- scripts/setup/ENV_script.sh
+++ scripts/setup/ENV_script.sh
@@ -11,6 +11,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-melodic-desktop-full -y
+sudo rm /etc/ros/rosdep/sources.list.d/* # clear the directory before init
 sudo rosdep init
 rosdep update
 
