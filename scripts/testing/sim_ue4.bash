57a2f667950f07dfd2ccb56c8baac0da401a0382
shibohan <tuotuogzs@gmail.com>
2020-03-03 14:54:03 +0000

    Move ue4_sim script to correct folder

--- scripts/testing/sim_ue4.bash
+++ scripts/testing/sim_ue4.bash
@@ -1,6 +1,20 @@
+#!/bin/bash
+killbg() {
+        for p in "${pids[@]}" ; do
+                kill "$p";
+        done
+}
+trap killbg EXIT
 cd ../../launch
-gnome-terminal -e "roslaunch ./step1SITLUE4.launch"
-echo "sleep 10s"
+pids=()
+# sim_vehicle doesn't work in background mode. Need to find a solution later
+# sim_vehicle.py -v ArduCopter &
+# pids+=($!)
+# sleep 15
+roslaunch ./step1SITLUE4.launch > step1log.txt  &
+pids+=($!)
 sleep 10
-gnome-terminal -e "roslaunch ./step2.launch"
-gnome-terminal -e "python ../scripts/simple_UDP.py"
+echo "UDP python"
+python ../scripts/simple_UDP.py & 
+pids+=($!)
+sleep infinity
