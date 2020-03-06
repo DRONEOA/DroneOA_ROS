55e3ed9708b1d73efc40568e02ab012517604af1
clair-hu <zhiqing.clair.hu@gmail.com>
2020-03-05 22:15:19 -0500

    Added unit test suite for CNCInterface
    58

---
+++ test/cnc_unittest.cpp
@@ -0,0 +1,35 @@
+/* Copyright (C) 2020 DroneOA Group - All Rights Reserved
+ * This file is part of DroneOA_ROS.
+ *
+ * DroneOA_ROS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation.
+ *
+ * DroneOA_ROS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with DroneOA_ROS. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Written by Zhiqing Hu <clair.hu.1@edu.uwaterloo.ca>, March 2020
+ */
+
+#include <gtest/gtest.h>
+#include <../include/droneoa_ros/CNCInterface.hpp>
+#include "ros/ros.h"
+// #include "mock_objects/"
+TEST(UtilTestSuite, takeoffSuccess) {
+    // TODO mock the call() method of ServiceClient
+    // gmock
+    
+}
+
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    // return RUN_ALL_TESTS();
+}
