6db2c2b0148874ead2bb72c098fc55e2f2077b2f
clair-hu <zhiqing.clair.hu@gmail.com>
2020-04-04 19:53:29 -0400

    test

--- test/cnc_unittest.cpp
+++ test/cnc_unittest.cpp
@@ -77,6 +77,7 @@ TEST(CNCTestSuite, takeoffSuccess) {
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    // testing::InitGoogleTest(&argc, argv);
+    // return RUN_ALL_TESTS();
+    ros::Rate a(10);
 }
