7761c795b69189fc31657b7d590169911071f9f9
shibohan <tuotuogzs@gmail.com>
2020-07-09 13:26:48 -0400

    pipeline test

--- scripts/ci/prescript.sh
+++ scripts/ci/prescript.sh
@@ -13,6 +13,10 @@ apt-get install ros-melodic-nodelet -y
 apt-get install ros-melodic-perception -y
 apt-get install ros-melodic-pcl-ros -y
 apt-get install ros-melodic-pcl-conversions -y
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+apt-get update
+apt-get install ros-melodic-ompl
 cd src
 git clone https://github.com/IntelRealSense/realsense-ros.git
 git clone http://gitlab.tuotuogzs.com/droneoa/ydlidar-x2l-local.git
