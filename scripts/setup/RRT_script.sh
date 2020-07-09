e98d35ceb075c0f62cfbce1296622fc6f10894cb
shibohan <tuotuogzs@gmail.com>
2020-07-09 19:33:44 -0400

    Add ros ompl wraper

--- scripts/setup/RRT_script.sh
+++ scripts/setup/RRT_script.sh
@@ -7,6 +7,11 @@ sudo chmod u+x install-ompl-ubuntu.sh
 ./install-ompl-ubuntu.sh
 sudo cp -r /usr/local/include/ompl-1.5/ompl /usr/local/include/ompl
 sudo rm -r /usr/local/include/ompl-1.5
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install ros-melodic-ompl -y
+sudo apt-get install libompl-dev -y
 echo "---------- Install FCL ----------"
 sudo apt install libccd-dev -y
 sudo apt install ros-melodic-octomap -y
